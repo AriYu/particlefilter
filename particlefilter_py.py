d7fb41641cbfbc53f3ea946b07dee8cd5b7e58eb
Arita Yuta <k104009y@mail.kyutech.jp>
2016-03-31 21:47:38 +0900

    add some functions

--- particlefilter_py.py
+++ particlefilter_py.py
@@ -15,15 +15,15 @@ RFID=numpy.array([[10.0, 0],
                   [0.0, 15.0],
                   [-5.0, 20],])
 
-def process_model(x, u, dt):
-    ret = [0.0] * STATE_DIMENTION
-    ret[0] = x[0] + u[0]*dt*math.cos(x[2])
-    ret[1] = x[1] + u[0]*dt*math.sin(x[2])
-    ret[2] = x[2] + u[1]*dt
+def process_model(x, u, delta_time):
+    ret = numpy.zeros((STATE_DIMENTION, 1))
+    ret[0] = x[0] + u[0]*delta_time*math.cos(x[2])
+    ret[1] = x[1] + u[0]*delta_time*math.sin(x[2])
+    ret[2] = x[2] + u[1]*delta_time
     return ret
 
 def control_model(time):
-    u = [0.0] * STATE_DIMENTION
+    u = numpy.zeros((2, 1))
     T = 10 #[sec]
     V = 1.0 #[m/s]
     yawrate = 5.0 #[deg/s]
@@ -50,7 +50,7 @@ class Particle:
     """A simple Particle class"""
 
     def __init__(self, dimention):
-        self.state = [0.0] * dimention
+        self.state = numpy.zeros((dimention, 1))
         self.weight = 0.0
 
     def print_particle(self):
@@ -65,6 +65,7 @@ class ParticleFilter:
         self.particles = [Particle(dimention)] * num_of_particles
         self.num_of_particles = num_of_particles
         self.dimention = dimention
+        self.estimation = None
 
     def print_info(self):
         print("Dimention is {}".format(self.dimention))
@@ -83,61 +84,83 @@ class ParticleFilter:
         self.observe_cov = cov
 
     def predict(self, dt, process_function, control):
-        process_noise = numpy.random.multivariate_normal(self.process_mean,
-                                                         self.process_cov,
-                                                         self.num_of_particles)
+        process_noise = numpy.random.normal(size=(self.num_of_particles, self.dimention, 1))
         for i, particle in enumerate(self.particles):
-            self.particles[i].state = process_function(particle.state, control, dt)
-            self.particles[i].state += process_noise[i]
+            self.particles[i].state = process_function(x=particle.state,
+                                                       u=control, delta_time=dt)
+            self.particles[i].state += (self.process_cov.dot(process_noise[i]))
 
     def sampling(self, observation, observe_function):
         for i, particle in enumerate(self.particles):
             self.particles[i].weight = observe_function(x=particle.state, y=observation)
-        
-    # def run(self):
-    #     for time in range(10):
-    #         print("time : {}".format(time))
-    #         self.sampling(time, process_model, control_model)
-    #         x = [x.state[0] for x in self.particles]
-    #         y = [x.state[1] for x in self.particles]
+        self.normalize()
+        self.resampling()
+        self.estimate()
+
+    def normalize(self):
+        sum_weight = sum([particle.weight for particle in self.particles])
+        if sum_weight != 0:
+            for i, particle in enumerate(self.particles):
+                self.particles[i].weight = self.particles[i].weight / sum_weight
+        else:
+            for i, particle in enumerate(self.particles):
+                self.particles[i].weight = 1.0 / float(self.num_of_particles)
+
+    def resampling(self):
+        pass
+
+    def estimate(self):
+        self.estimation = numpy.zeros((self.dimention, 1))
+        for particle in (self.particles):
+            self.estimation += particle.state * particle.weight
+            
 
 
 if __name__ == "__main__":
     particlefilter = ParticleFilter(dimention=STATE_DIMENTION, num_of_particles=100)
     process_mean = numpy.array([0, 0, 0])
-    process_cov = numpy.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    process_cov = numpy.array([[0.2, 0, 0], [0, 0.2, 0], [0, 0, math.radians(0.1)]])**2
     particlefilter.set_process_noise_param(mean=process_mean, cov=process_cov)
     observe_mean = numpy.array([0])
     observe_cov = numpy.array([1])
     particlefilter.set_observe_noise_param(mean=observe_mean, cov=observe_cov)
     particlefilter.print_info()
+    # Simulation parameter
+    simulation_cov = numpy.array([[0.2, 0], [0, math.radians(0.1)]])
+
     truth_state = numpy.zeros((3, 1))
     deadr_state = numpy.zeros((3, 1))
     control = numpy.zeros((1, 2))
     observation = numpy.zeros((len(RFID), 1))
     dt = 0.5
     time = 0.0
-    trajectory = []
-    num_of_loop = 1000
+    truth_trajectory = []
+    deadr_trajectory = []
+    num_of_loop = 200
     pbar = ProgressBar(widgets=[Percentage(), Bar()], max_value=num_of_loop)
     for i in range(num_of_loop):
         # calculation ground truth
         time = time + dt
         control = control_model(time)
-        truth_state = process_model(truth_state, control, dt)
-        deadr_state = process_model(deadr_state,
-                                    (control
-                                     +numpy.random.multivariate_normal(process_mean,
-                                                                       process_cov)),dt)
+        truth_state = process_model(x=truth_state, u=control, delta_time=dt)
+        deadr_state = process_model(x=deadr_state,
+                                    u=(control
+                                       +simulation_cov.dot(numpy.random.randn(2,1))),
+                                    delta_time=dt)
         particlefilter.predict(dt, process_model, control)
         for ind , rfid in enumerate(RFID):
             observation[ind] = (numpy.linalg.norm(RFID[ind] - truth_state[:2])
                                 + numpy.random.normal(observe_mean[0], observe_cov[0]))
         particlefilter.sampling(observation, observe_model)
-        trajectory.append(truth_state)
+        truth_trajectory.append(truth_state)
+        deadr_trajectory.append(deadr_state)
         pbar.update(i+1)
+
     pbar.finish()
     plt.figure(facecolor="w")
-    plt.scatter([x[0] for x in trajectory], [x[1] for x in trajectory], color='r', marker='x')
+    plt.scatter([x[0] for x in truth_trajectory], [x[1] for x in truth_trajectory],
+                color='r', marker='x')
+    plt.scatter([x[0] for x in deadr_trajectory], [x[1] for x in deadr_trajectory],
+                color='b', marker='x')
     plt.axis([-30, 30.0, 0.0, 30.0])
     plt.show()
